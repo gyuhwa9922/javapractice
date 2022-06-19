c491a91a4ff402579df8a37d8d7ca4b7bb689be4
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-06-19 23:47:39 +0900

    Create main.java

---
+++ Student/src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+import viewer.StuViewer;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		StuViewer stuviewer = new StuViewer();
+		stuviewer.showMenu();
+		
+	}
+
+}
